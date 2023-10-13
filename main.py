e3f86d6ae7f53b45d3ffefa473bffefcb65ad346
Bluecoolclouds <49954845+Bluecoolclouds@users.noreply.github.com>
2023-10-13 20:08:42 +0400

    Update main.py

--- main.py
+++ main.py
@@ -6,41 +6,19 @@ import os
 from flask import Flask, request
 from telegram.ext import Dispatcher, MessageHandler, Filters
 
-telegram_bot_token = str(os.getenv("6556995841:AAGyBXCCbremRqXdHHuARfR3yaabQH78v-MN"))
+telegram_bot_token = str(os.getenv("6556995841:AAE4ZccxFlpMzEtf1ZLGaDsbE13RYD-95D0"))
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Initial Flask app
-app = Flask(__name__)
 
-# Initial bot by Telegram access token
-bot = telegram.Bot(token=telegram_bot_token)
-
-@app.route('/callback', methods=['POST'])
-def webhook_handler():
-    """Set route /hook with POST method will trigger this method."""
-    if request.method == "POST":
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
-
-        # Update dispatcher process that handler to process this message
-        dispatcher.process_update(update)
-    return 'ok'
+@bot.message_handler(commands=['start'])
+def start_command(message):
+  bot.send_message(message.chat.id, f"Цена на рублей")
 
 
-def reply_handler(bot, update):
-    """Reply message."""
-    text = update.message.text
-    update.message.reply_text(text)
-
-# New a dispatcher for bot
-dispatcher = Dispatcher(bot, None)
-
-# Add handler for handling message, there are many kinds of message. For this handler, it particular handle text
-# message.
-dispatcher.add_handler(MessageHandler(Filters.text, reply_handler))
 
 if __name__ == "__main__":
     # Running server
